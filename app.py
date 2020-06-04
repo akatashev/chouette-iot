f3d6160d945853c0ac18d0ad27d08a6d293586fa
Artem Katashev <artem@cainthus.com>
2020-06-04 19:32:32 +0100

    app: Reformat app.py

--- app.py
+++ app.py
@@ -3,64 +3,68 @@ import sys
 import time
 
 import gevent.monkey  # type: ignore
+gevent.monkey.patch_all()
 from pythonjsonlogger import jsonlogger  # type: ignore
 
-gevent.monkey.patch_all()
-from pykka import ActorRegistry  # type: ignore
+from chouette._singleton_actor import SingletonActor
 
-from chouette import Scheduler, ChouetteConfig
+from pykka import ActorRegistry  # type: ignore
+from typing import List, Type
+
+from chouette import Scheduler, ChouetteConfig, Cancellable
 from chouette.metrics import MetricsCollector, MetricsAggregator, MetricsSender
 
 logger = logging.getLogger("chouette")
 
 
-def setup_logging(log_level):
-    """
-    Configures logger to use JSON format and support dramatiq messages,
-    sets the desired log level.
-    :return: None
-    """
-    #: Setup logging
-    stdout_handler = logging.StreamHandler(sys.stdout)
+class Chouette:
+    @staticmethod
+    def setup_logging(log_level: str) -> None:
+        """
+        Configures logger to use JSON format and set s the desired log level.
 
-    json_formatter = jsonlogger.JsonFormatter(
-        fmt="%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%dT%H:%M:%S%z"
-    )
+        Returns: None
+        """
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        json_formatter = jsonlogger.JsonFormatter(
+            fmt="%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%dT%H:%M:%S%z"
+        )
+        stdout_handler.setFormatter(json_formatter)
+        logging.basicConfig(level=log_level, handlers=[stdout_handler])
 
-    stdout_handler.setFormatter(json_formatter)
+    @staticmethod
+    def schedule_call(
+            interval, actor_class: Type[SingletonActor], message
+    ) -> Cancellable:
+        actor_ref = actor_class.get_instance()
+        initial_delay = interval - (time.time() % interval)
+        timer = Scheduler.schedule_at_fixed_rate(
+            initial_delay, interval, actor_ref.tell, message
+        )
+        return timer
 
-    # Setup the Basic application logger
-    logging.basicConfig(level=log_level, handlers=[stdout_handler])
+    @classmethod
+    def run(cls) -> List[Cancellable]:
+        timers = []
+        config = ChouetteConfig()
+        cls.setup_logging(config.log_level)
+        # Sender actor:
+        timers.append(cls.schedule_call(config.release_interval, MetricsSender, "send"))
+        # Aggregator actor:
+        timers.append(
+            cls.schedule_call(config.aggregate_interval, MetricsAggregator, "aggregate")
+        )
+        # Collector actor:
+        if not config.collector_plugins:
+            return timers
+        timers.append(
+            cls.schedule_call(config.capture_interval, MetricsCollector, "collect")
+        )
+        return timers
 
 
 if __name__ == "__main__":
-    """
-    TODO: REFACTOR, REFACTOR, REFACTOR.
-    """
-    config = ChouetteConfig()
-    setup_logging(config.log_level)
-    collector = MetricsCollector.start()
-    aggregator = MetricsAggregator.start()
-    sender = MetricsSender.start()
-    timers = []
-    initial_delay = 60 - (time.time() % 60)
-    if config.collector_plugins:
-        collector = MetricsCollector.start()
-        timers.append(
-            Scheduler.schedule_at_fixed_rate(
-                initial_delay, config.capture_interval, collector.tell, "collect"
-            )
-        )
-    timers.append(
-        Scheduler.schedule_at_fixed_rate(
-            initial_delay, config.aggregate_interval, aggregator.tell, "aggregate"
-        )
-    )
-    timers.append(
-        Scheduler.schedule_at_fixed_rate(
-            initial_delay, config.release_interval, sender.tell, "send"
-        )
-    )
+    timers = Chouette.run()
     try:
         while True:
             time.sleep(1)
